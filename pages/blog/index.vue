dddac45f1a308058ed48a2fe0b9e0380e4221694
pengjian <pengjian@sobug.com>
2018-11-15 21:43:52 +0800

    feat: 测试其他router

---
+++ pages/blog/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        blog
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/mysql/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        mysql
+    </div>
+</template>
+
+<style scoped>
+
+</style>
