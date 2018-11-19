1f74fff8ad147d7ebef3b6b681cceb09315efdb5
pengjian <jamesjianpeng@gmail.com>
2018-11-19 13:15:02 +0800

    fix: cv 修改

--- components/cv/index.vue
+++ components/cv/index.vue
@@ -322,40 +322,8 @@ export default {
 }
 </script>
 
-<style>
-@import url("./style/cormalize.css");
-@import url("./style/main.css");
-@import url("./style/response.css");
-/* a {
-  text-decoration: none;
-}
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-} */
+<style scoped>
+@import url("~/components/cv/style/normalize.css");
+@import url("~/components/cv/style/main.css");
+@import url("~/components/cv/style/response.css");
 </style>
