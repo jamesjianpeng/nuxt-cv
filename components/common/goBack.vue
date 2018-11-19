7176f66c01be288d82391758c53f3c1a7dbe3b02
pengjian <jamesjianpeng@gmail.com>
2018-11-19 13:11:05 +0800

    feat: 返回 home 的按钮

---
+++ components/common/goBack.vue
@@ -0,0 +1,32 @@
+<template>
+    <button class="bnt pointer" @click="goback">返回</button>
+</template>
+
+<script>
+export default {
+    name: 'GoBack',
+    methods: {
+        goback() {
+            this.$router.push({ path: '/home' })
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+$mainColorLigt: #48a999;
+$mainColor: #004c40;
+
+.bnt {
+    border: none;
+    background: #fff;
+    color: $mainColorLigt;
+    font-size: 14px;
+    outline:none;
+    &:hover {
+        color: $mainColor;
+    }
+}
+</style>
+
+
