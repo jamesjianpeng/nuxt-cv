2b54ff15356fc2b1129deba413b726c04b664163
pengjian <jamesjianpeng@gmail.com>
2018-11-19 13:22:32 +0800

    perf: CV 如何问价和 layouts index 记录

---
+++ pages/css/layouts/index.vue
@@ -0,0 +1,65 @@
+<template>
+    <div>
+        <div class="home">
+            CssLayoutsIndex <GoBack />
+            <ul class="item-content">
+                <li class="item">
+                    <nuxt-link to="/css/layouts" class="nav-text">Layouts</nuxt-link>
+                    <ul class="sub-item-content">
+                        <li class="sub-item">
+                            <nuxt-link to="/css/layouts/float" class="nav-text">各种居中</nuxt-link>
+                        </li>
+                        <li class="sub-item">
+                            <nuxt-link to="/css/layouts/float" class="nav-text">Layouts float</nuxt-link>
+                        </li>
+                        <li class="sub-item">
+                            <nuxt-link to="/css/layouts/flex" class="nav-text">layouts flex</nuxt-link>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+/**
+ * @description css layouts index 
+ * @todo 将要废弃
+ */
+import GoBack from '~/components/common/goBack.vue'
+export default {
+    name: 'CssLayoutsIndex',
+    components: {
+        GoBack
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+$mainColorLigt: #48a999;
+$mainColor: #004c40;
+$marginTopNormal: 15px;
+$marginTopSmall: 8px;
+
+.home {
+    .item-content {
+        .item {
+            margin-top: $marginTopNormal;
+        }
+        .item > a {
+        }
+    }
+    .sub-item-content {
+        margin-top: $marginTopSmall;
+        .sub-item > a {
+        }
+    }
+    .nav-text {
+        color: $mainColorLigt;
+        &:hover {
+            color: $mainColor;
+        }
+    }
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,5 @@
 <template>
   <section class="container">
-    <!-- <div>
-      <logo/>
-      <h1 class="title">
-        James <a href="https://www.pengjiandry.com">彭涧的博客</a>
-      </h1>
-    </div> -->
     <cv />
   </section>
 </template>
@@ -20,36 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-/* .container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-} */
-</style>
