e3ebb6b269cb5ba3e4c84733477348b0130d7d7b
pengjian <jamesjianpeng@gmail.com>
2018-11-19 13:23:58 +0800

    perf: css index 入口

---
+++ pages/css/icons/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        CssIconsIndex
+        <GoBack />
+        <ul>
+            <li>
+                <james-share />
+            </li>
+            <li>
+                <james-magnifier />
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+/**
+ * @description css inons 组件入口
+ * @todo 这块暂时没有想好
+ */
+import JamesShare from '~/components/css_icons/share.vue'
+import JamesMagnifier from '~/components/css_icons/magnifier.vue'
+import GoBack from '~/components/common/goBack.vue'
+
+export default {
+    name: 'CssIconsIndex',
+    components: {
+        JamesShare,
+        JamesMagnifier,
+        GoBack
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
---
+++ pages/css/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CssIndex',
+    mounted() {
+        this.$router.push({ path: '/css/icons' })
+    }
+}
+</script>
+
+<style>
+
+</style>
+
+
