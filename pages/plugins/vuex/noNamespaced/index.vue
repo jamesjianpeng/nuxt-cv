7254b5bc2460e113c6e47ad6e9dc8818e7e45342
pengjian <jamesjianpeng@gmail.com>
2018-11-22 13:08:22 +0800

    perf: 添加 返回按钮

--- pages/plugins/vuex/noNamespaced/index.vue
+++ pages/plugins/vuex/noNamespaced/index.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
+        <GoBack />
         <VuexNoNamespaced />
     </div>
 </template>
 
 <script>
 import VuexNoNamespaced from '~/components/plugins_vuex/no_namespaced.vue'
+import GoBack from '~/components/common/goBack.vue'
 
 export default {
     name: 'VuexNoNamespacedIndex',
     components: {
-        VuexNoNamespaced
+        VuexNoNamespaced,
+        GoBack
     }
 }
 </script>
