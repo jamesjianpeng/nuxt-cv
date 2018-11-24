364f3ae93a4ddecf5e542264cbe2452284d06c2f
pengjian <jamesjianpeng@gmail.com>
2018-11-24 12:09:33 +0800

    feat: 特定属性选择类型

---
+++ components/css_priority/attribute_selector_special.vue
@@ -0,0 +1,93 @@
+<template>
+    <div>
+        <div>
+            <i class="no">1</i>
+            <div>
+                <span title="five-1-1" lang="ch">
+                    属性的值只有一个，这个值等于或者从头可以匹配 five-1
+                </span>
+            </div>
+            <div>
+                <span title="five-1 text" lang="ch">
+                    特殊选择器不匹配1
+                </span>
+            </div>
+            <div>
+                <span title="five-1-1 text" lang="ch">
+                    有多个值的并且 five-1 在这些值没有相等的同时是第一个值从开头匹配的
+                </span>
+            </div>
+            <div>
+                <span title="five-1">
+                    属性的值只有一个，这个值等于或者从头可以匹配 five-1
+                </span>
+            </div>
+        </div>
+        <pre>
+html:
+&lt;div&gt;
+    &lt;span title="five-1-1" lang="ch"&gt;
+        属性的值只有一个，这个值等于或者从头可以匹配 five-1
+    &lt;/span&gt;
+&lt;/div&gt;
+&lt;div&gt;
+    &lt;span title="five-1 text" lang="ch"&gt;
+        特殊选择器不匹配1
+    &lt;/span&gt;
+&lt;/div&gt;
+&lt;div&gt;
+    &lt;span title="five-1-1 text" lang="ch"&gt;
+        有多个值的并且 five-1 在这些值没有相等的同时是第一个值从开头匹配的
+    &lt;/span&gt;
+&lt;/div&gt;
+&lt;div&gt;
+    &lt;span title="five-1"&gt;
+        属性的值只有一个，这个值等于或者从头可以匹配 five-1
+    &lt;/span&gt;
+&lt;/div&gt;
+
+css
+[title|="five-1"] { 
+    /*
+     * 0.0.1.0 title 属性的值只有一个，这个值等于或者从头可以匹配 five-1 或者
+     * 有多个值的并且 five-1 在这些值没有相等的同时是第一个值从开头匹配的
+     */ 
+    color: red;
+}
+        </pre>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "attributeSelectorSpecial"
+}
+</script>
+
+<style lang="scss" scoped>
+$mainColorLigt: #48a999;
+$layoutBgColor: #009688;
+$mainColor: #004c40;
+$blockBgColor: #c67100;
+$inlineColor: #6ec6ff;
+$hrColor: #ffd54f;
+$bgColor: #6ec6ff;
+$textColoe: #fff;
+$marginNormal: 15px;
+$marginSmall: 8px;
+$height: 38px;
+
+// [title|="five"] { // 0.0.1.0
+//     color: red;
+// }
+
+[title|="five-1"] { 
+    /*
+     * 0.0.1.0 title 属性的值只有一个，这个值等于或者从头可以匹配 five-1 或者
+     * 有多个值的并且 five-1 在这些值没有相等的同时是第一个值从开头匹配的
+     */ 
+    color: red;
+}
+</style>
+
+
