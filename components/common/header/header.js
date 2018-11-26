a92ff6df6455946fc7bc01b8217d2116f06963ab
pengjian <jamesjianpeng@gmail.com>
2018-11-26 11:56:05 +0800

    feat: 添加子路由

--- components/common/header/header.js
+++ components/common/header/header.js
@@ -2,21 +2,58 @@ export default [
     {
         name: 'HOME',
         path: '/home',
+        child: [],
     },
     {
         name: 'CSS',
         path: '/css',
+        child: [
+            {
+                name: '布局',
+                path: '/css/layouts',
+            },
+            {
+                name: '选择器/特殊性',
+                path: '/css/priority',
+            },
+        ],
     },
     {
         name: 'JavaScript',
-        path: '/oo',
+        path: '/JavaScript',
+        child: [
+            {
+                name: 'oo',
+                path: '/oo',
+            },
+        ],
     },
     {
         name: 'MysqlPractice',
         path: '/mysqlPractice',
+        child: [
+            {
+                name:'',
+                path:'',
+            },
+        ],
     },
     {
         name: 'plugins',
         path: '/plugins',
+        child: [
+            {
+                name:'axios test',
+                path:'/plugins/axios',
+            },
+            {
+                name:'vuex test',
+                path:'/plugins/vuex',
+            },
+            {
+                name:'lifeCycle',
+                path:'/plugins/lifeCycle',
+            },
+        ],
     },
 ]
